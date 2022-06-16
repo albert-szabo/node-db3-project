1866a9eafe962c1f8ec3a7fcd5ad5cdbb2f4218c
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-15 19:32:53 -0700

    Completed findById database access function

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -24,7 +24,7 @@ function find() {
     .select('sc.*')
     .count('st.step_id as number_of_steps')
     .groupBy('sc.scheme_id')
-    .orderBy('sc.scheme_id asc')
+    .orderBy('sc.scheme_id');
 }
 
 // EXERCISE B
@@ -94,8 +94,30 @@ function find() {
       }
   */
 
-function findById(scheme_id) {
+async function findById(scheme_id) {
+  const rows = await database('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .where('sc.scheme_id', scheme_id)
+    .select('sc.scheme_name', 'st.*', 'sc.scheme_id')
+    .orderBy('st.step_number');
 
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: []
+  };
+
+  rows.forEach(row => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions
+      });
+    }
+  });
+
+  return result;
 }
 
 // EXERCISE C
